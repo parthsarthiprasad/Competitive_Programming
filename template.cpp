a110c524ed2fecfdd6ac1a4f2bd58c0405c0567f
parth sarthi prasad <mepsp2000@gmail.com>
2020-05-31 05:30:00 +0530

    aded template

---
+++ template.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+ 
+const double pi=acos(-1.0);
+using namespace std;
+#define   endl    '\n'
+#define   sl(n)     scanf("%lld",&n)
+#define   mp      make_pair
+#define   pb      push_back
+#define   ppb     pop_back
+#define   fi      first
+#define   se      second
+#define   ll      long long
+#define   ull     unsigned long long
+#define   ld      long double
+#define   pii     pair<int, int>
+#define   f(i,a,b)  for(ll i = (ll)(a); i < (ll)(b); i++)
+#define   rf(i,a,b)   for(ll i = (ll)(a); i > (ll)(b); i--)
+#define   ms(a,b)   memset((a),(b),sizeof(a))
+#define   abs(x)    ((x<0)?(-(x)):(x))
+#define   MAX     200005
+#define   inf     LLONG_MAX
+#define   ninf    LLONG_MIN
+#define   MIN     INT_MIN
+#define   yeet    return 0;
+#define tihs if(fopen("input.txt","r"))freopen("input.txt", "r", stdin),freopen("output.txt", "w", stdout);
+ 
+#define fast_io ios_base::sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0) ;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+ 
+inline long long  MAX2(long long  a, long long  b){return (a)>(b)?(a):(b);}
+inline long long  MAX3(long long  a, long long  b,long long  c){return (a)>(b)?((a)>(c)?(a):(c)):((b)>(c)?(b):(c));}
+inline long long  MIN2(long long  a, long long  b){return (a)<(b)?(a):(b);}
+inline long long  MIN3(long long  a, long long b,long long c){return (a)<(b)?((a)<(c)?(a):(c)):((b)<(c)?(b):(c));}
+ 
+ 
+int mod = 1e9 +7 ;
+ 
+ 
+////****************************************************************************************************************************************************************************************************************////
+
+ 
+ int main(){
+     tihs;
+     int n;
+     cin>>n;
+     
+     }
+ 
+ 
