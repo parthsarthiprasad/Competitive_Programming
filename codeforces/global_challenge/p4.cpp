e058a83a10dfb093854edc297bf43c8683f4c3c1
parth sarthi prasad <mepsp2000@gmail.com>
2020-06-25 05:30:00 +0530

    added 4th question implementation

---
+++ codeforces/global_challenge/p4.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+#define pb push_back
+#define for(a,b) for(int i=a;i<b;i++)
+#define pii pair<int,int>
+#define pll pair<long, long>
+int solve(){
+    
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--){
+        cout<<solve()<<endl;
+    }
+}
